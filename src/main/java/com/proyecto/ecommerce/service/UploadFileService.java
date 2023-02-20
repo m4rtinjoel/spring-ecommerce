316c3110603804617bce4f79dcfbec4966327c37
m4rtinjoel <m4rtinjl@gmail.com>
2023-02-19 20:09:20 -0600

    Clase de servicio para subir una imagen al servidor

---
+++ src/main/java/com/proyecto/ecommerce/service/UploadFileService.java
@@ -0,0 +1,30 @@
+package com.proyecto.ecommerce.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+@Service
+public class UploadFileService {
+	private String folder="images//";
+	
+	public String saveImage(MultipartFile file) throws IOException {
+		if(!file.isEmpty()) {
+			byte [] bytes=file.getBytes();
+			Path path=Paths.get(folder+file.getOriginalFilename());
+			Files.write(path, bytes);
+			return file.getOriginalFilename();
+		}
+		return "default.jpg"; 
+	}
+	
+	public void delete(String name) {
+		String ruta="images//";
+		File file=new File(ruta+name);
+		file.delete();
+		 
+	}
+}
