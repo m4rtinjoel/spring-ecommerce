d40a255dc1ce2b1564000274a6e2266c81016a4e
m4rtinjoel <m4rtinjl@gmail.com>
2023-02-21 20:57:02 -0600

    funcionalidad para eliminar un producto del carrito de compras

--- src/main/java/com/proyecto/ecommerce/controller/HomeController.java
+++ src/main/java/com/proyecto/ecommerce/controller/HomeController.java
@@ -19,65 +19,87 @@ import com.proyecto.ecommerce.service.ProductoService;
 @Controller
 @RequestMapping("/")
 public class HomeController {
-	
-	private final Logger log=LoggerFactory.getLogger(HomeController.class); 
+
+	private final Logger log = LoggerFactory.getLogger(HomeController.class);
 
 	@Autowired
 	private ProductoService productoService;
-	
-	//para almacenar los detalles de la orden
-	private List<DetalleOrden> detalles=new ArrayList<DetalleOrden>();
-	
-	//datos de la orden
-	Orden orden=new Orden();
-	
+
+	// para almacenar los detalles de la orden
+	private List<DetalleOrden> detalles = new ArrayList<DetalleOrden>();
+
+	// datos de la orden
+	Orden orden = new Orden();
+
 	@GetMapping("")
 	public String home(Model model) {
-		model.addAttribute("productos",productoService.findAll());
+		model.addAttribute("productos", productoService.findAll());
 		return "usuario/home";
 	}
-	
+
 	@GetMapping("productohome/{id}")
 	public String productoHome(@PathVariable Integer id, Model model) {
-		log.info("id producto enviado como parametro {}",id );
-		Producto producto =new Producto();
-		Optional<Producto> productoOptional=productoService.get(id);
+		log.info("id producto enviado como parametro {}", id);
+		Producto producto = new Producto();
+		Optional<Producto> productoOptional = productoService.get(id);
 		producto = productoOptional.get();
-		
+
 		model.addAttribute("producto", producto);
 		return "usuario/productohome";
 	}
-	
+
 	@PostMapping("/cart")
-	public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad,Model model) {
-		DetalleOrden detalleOrden=new DetalleOrden();
-		Producto producto=new Producto();
-		double sumaTotal=0;	
-		
-		Optional<Producto> optionalProducto=productoService.get(id);
-		log.info("Producto añadido: {}",optionalProducto.get());
-		log.info("Cantidad: {}",cantidad);
-		
-		producto=optionalProducto.get();
-		
+	public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad, Model model) {
+		DetalleOrden detalleOrden = new DetalleOrden();
+		Producto producto = new Producto();
+		double sumaTotal = 0;
+
+		Optional<Producto> optionalProducto = productoService.get(id);
+		log.info("Producto añadido: {}", optionalProducto.get());
+		log.info("Cantidad: {}", cantidad);
+
+		producto = optionalProducto.get();
+
 		detalleOrden.setCantidad(cantidad);
 		detalleOrden.setPrecio(producto.getPrecio());
 		detalleOrden.setNombre(producto.getNombre());
-		detalleOrden.setTotal(producto.getPrecio()*cantidad);
+		detalleOrden.setTotal(producto.getPrecio() * cantidad);
 		detalleOrden.setProducto(producto);
 		detalles.add(detalleOrden);
-		
-		sumaTotal=detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
-		
+
+		sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
+
 		orden.setTotal(sumaTotal);
-		
-		model.addAttribute("cart",detalles);
-		
-		
-		
-		
-		model.addAttribute("orden",orden);
-		
+
+		model.addAttribute("cart", detalles);
+
+		model.addAttribute("orden", orden);
+
+		return "usuario/carrito";
+	}
+
+	// quitar un producto del carrito
+	@GetMapping("/delete/cart/{id}")
+	public String deteleProductoCart(@PathVariable Integer id, Model model) {
+
+		// Lista nueva de productos
+		List<DetalleOrden> ordenesNueva = new ArrayList<DetalleOrden>();
+		for (DetalleOrden detalleOrden : detalles) {
+			if (detalleOrden.getProducto().getId() != id) {
+				ordenesNueva.add(detalleOrden);
+			}
+		}
+		// Poner la nueva lista con los productos restantes
+		detalles = ordenesNueva;
+
+		double sumaTotal = 0;
+		sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
+
+		orden.setTotal(sumaTotal);
+
+		model.addAttribute("cart", detalles);
+
+		model.addAttribute("orden", orden);
 		return "usuario/carrito";
 	}
 }
