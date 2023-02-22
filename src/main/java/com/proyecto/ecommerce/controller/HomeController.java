5db2b247f36c9d534e2a053708d5f35df555a91d
m4rtinjoel <m4rtinjl@gmail.com>
2023-02-21 21:29:00 -0600

    añadimos la redirección a la vista resumen orden

--- src/main/java/com/proyecto/ecommerce/controller/HomeController.java
+++ src/main/java/com/proyecto/ecommerce/controller/HomeController.java
@@ -116,4 +116,9 @@ public class HomeController {
 		model.addAttribute("orden", orden);
 		return "usuario/carrito";
 	}
+	
+	@GetMapping("/order")
+	public String order() {
+		return "usuario/resumenorden";
+	}
 }
