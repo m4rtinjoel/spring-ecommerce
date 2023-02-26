0109a12d44648d0d48806d441c267b92f816ef72
m4rtinjoel <m4rtinjl@gmail.com>
2023-02-25 21:08:15 -0600

    implementar template diferente para usuario logueado

--- src/main/java/com/proyecto/ecommerce/controller/HomeController.java
+++ src/main/java/com/proyecto/ecommerce/controller/HomeController.java
@@ -47,6 +47,8 @@ public class HomeController {
 	public String home(Model model, HttpSession session) {
 		log.info("Session del usuario: {}",session.getAttribute("idusuario"));
 		model.addAttribute("productos", productoService.findAll());
+		//session 
+		model.addAttribute("sesion", session.getAttribute("idusuario"));
 		return "usuario/home";
 	}
 
@@ -124,9 +126,13 @@ public class HomeController {
 	}
 	
 	@GetMapping("getcart")
-	public String getCart(Model model) {
+	public String getCart(Model model, HttpSession session) {
 		model.addAttribute("cart", detalles);
 		model.addAttribute("orden", orden);
+		
+		//session
+		model.addAttribute("sesion",session.getAttribute("idusuario"));
+		
 		return "usuario/carrito";
 	}
 	
